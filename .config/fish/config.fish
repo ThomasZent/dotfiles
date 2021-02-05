7f079b5d4fc4a2ce371d09cba4bff6fa61813ee0
Thomas Zentmyer <thomas.zent@gmail.com>
2021-02-04 23:04:51 -0800

    Add doas-retry function to fish for when I forget doas

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,8 @@ set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      block
 
+function doas!
+    eval doas $history[1]
+end
+
 starship init fish | source
